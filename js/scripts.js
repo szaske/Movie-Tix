a671bd4f5b3220990a36ebe86ff57016e12f626b
Steve Zaske <github@zaske.com>
2017-06-13 10:10:13 -0700

    include auto-generated movie showings drop down UI element

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,41 @@
+//BUSINESS LOGIC
+function Showing(movieName, time, firstRun) {
+  this.movieName = movieName;
+  this.time = time;
+  this.firstRun = firstRun;
+}
 
+function PurchaseOrder(showing, tickets) {
+  this.showing = showing;
+  this.tickets = [];
+}
 
+var getFormattedTime = function (fourDigitTime) {
+    var hours24 = parseInt(fourDigitTime.substring(0, 2),10);
+    var hours = ((hours24 + 11) % 12) + 1;
+    var amPm = hours24 > 11 ? 'pm' : 'am';
+    var minutes = fourDigitTime.substring(2);
 
+    return hours + ':' + minutes + amPm;
+};
+
+var movieShows = [
+  {movieName: "Bloodbath 4", time: 1200, firstRun: false},
+  {movieName: "Wonder Woman", time: 1400, firstRun: true},
+  {movieName: "Snakes in Space", time: 1600, firstRun: false}
+];
+
+//UI LOGIC
 $(document).ready(function() {
+  // <select id="movie-Showings" class="input form-control address-type">
+  //   <option data-time="1200" value="Bloodbath-4">Bloodbath 4, 12:00pm</option>
+  //   <option data-time="1400" value="Bloodbath-4">Bloodbath 4, 2:00pm</option>
+  // </select>
+  console.log(getFormattedTime('1450'));
+  movieShows.forEach(function(movieShow) {
+    $("#movie-Showings").append("<option data-time='1200' value='" + movieShow.movieName + "'>" + movieShow.movieName + ", " + getFormattedTime(movieShow.time.toString())+"</option>)");
+  });
+
 
   //validation on input text field
   $('.input').on('input', function() {
