97f50707e2472a34bdd70ead569c389c33a0387c
Steve Zaske <github@zaske.com>
2017-06-13 14:52:22 -0700

    add working receipt math, tax, and price per ticket

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,6 @@ function PurchaseOrder(showing, tickets) {
   this.tickets = tickets;
 }
 
-// function Ticket(type,show) {
-// this.type = type;
-// this.show = show;
-// }
-
 var getFormattedTime = function (fourDigitTime) {
     var hours24 = parseInt(fourDigitTime.substring(0, 2),10);
     var hours = ((hours24 + 11) % 12) + 1;
@@ -25,20 +20,52 @@ var getFormattedTime = function (fourDigitTime) {
     return hours + ':' + minutes + amPm;
 };
 
+//re-formats a number as $$
+function showMeTheMoney(num) {
+  return "$"+num.toFixed(2).toString();
+}
+
 function Prices(po) {
-  var kidTix = 0;
-  var adultTix = 0;
-  var seniorTix = 0;
-  kidTix = po.tickets.toString().match(/kid/g).length; //we're smurt
-  adultTix = po.tickets.toString().match(/adult/g).length; //we're smurt
-  seniorTix = po.tickets.toString().match(/senior/g).length; //we're smurt
-  console.log(kidTix, adultTix, seniorTix);
+  //Variables
+  var ticketPrice = 12.50;
+  var taxPercent = .098;
+  var totalPrice = 0;
+  var kidTotal = 0;
+  var adultTotal = 0;
+  var seniorTotal = 0;
+  var taxTotal = 0;
+  var totalTotal = 0;
+
+  // Adjust price for first run movies
+  po.showing.firstRun === true ? ticketPrice+=2.5: ticketPrice=ticketPrice;
+
+  // Adjust price for first run movies
+  po.showing.time < 11 ? ticketPrice-=3: ticketPrice=ticketPrice;
+
+  //determine total kid ticket prices
+  if (po.tickets.toString().match(/kid/g) != null) {
+    kidTotal = po.tickets.toString().match(/kid/g).length * (ticketPrice*.8);
+  }
+  //determine total adult ticket prices
+  if (po.tickets.toString().match(/adu/g) != null) {
+    adultTotal = po.tickets.toString().match(/adu/g).length * ticketPrice;}
+  //determine total senior ticket prices
+  if (po.tickets.toString().match(/sen/g) != null) {
+    seniorTotal = po.tickets.toString().match(/sen/g).length * (ticketPrice*.9);}
+
+  taxTotal= (kidTotal+adultTotal+seniorTotal)*taxPercent;
+  totalTotal=(kidTotal+adultTotal+seniorTotal)+taxTotal;
+
+
+  return [showMeTheMoney(kidTotal), showMeTheMoney(adultTotal), showMeTheMoney(seniorTotal), showMeTheMoney(taxTotal), showMeTheMoney(totalTotal), showMeTheMoney(ticketPrice*.8), showMeTheMoney(ticketPrice), showMeTheMoney(ticketPrice*.9) ];
 }
 
 var movieShows = [
-  {id:0, movieName: "Bloodbath 4", time: 1200, firstRun: false},
-  {id:1, movieName: "Wonder Woman", time: 1400, firstRun: true},
-  {id:2, movieName: "Snakes in Space", time: 1600, firstRun: false}
+  {id:0, movieName: "Bloodbath 4", time: 1000, firstRun: false},
+  {id:0, movieName: "Bloodbath 4", time: 2300, firstRun: false},
+  {id:1, movieName: "Wonder Woman", time: 1100, firstRun: true},
+  {id:2, movieName: "Wonder Woman", time: 1400, firstRun: true},
+  {id:3, movieName: "Snakes in Space", time: 1600, firstRun: false}
 ];
 
 //empty array for all showings
@@ -83,8 +110,23 @@ $(document).ready(function() {
       // create purchase order from
     }
 
-    //create a showing
-    Prices(new PurchaseOrder(shows[showId], purchasedTix));
+    //create our numbers to be shown in HTML
+    var output = Prices(new PurchaseOrder(shows[showId], purchasedTix));
+    $("#kid-Total").text(output[0]);
+    $("#adult-Total").text(output[1]);
+    $("#senior-Total").text(output[2]);
+    $("#tax-Total").text(output[3]);
+    $("#total-Total").text(output[4]);
+
+    $("#row-Kid-Count").text(numOfChildTix);
+    $("#row-Adult-Count").text(numOfAdultTix);
+    $("#row-Senior-Count").text(numOfSeniorTix);
+
+    $("#row-Kid-Price").text(output[5]);
+    $("#row-Adult-Price").text(output[6]);
+    $("#row-Senior-Price").text(output[7]);
+
+    console.log(output);
 
     //create a purchase order from array of tickets
 
