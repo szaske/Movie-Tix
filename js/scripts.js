3f80b5779cf4abe4b94cdca0f5634b94532d90f2
Steve Zaske <github@zaske.com>
2017-06-13 09:28:25 -0700

    add purchase button and event listener

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
+
+
+
 $(document).ready(function() {
 
-console.log($("#test").data("time"));
+  //validation on input text field
+  $('.input').on('input', function() {
+    console.log("what?",this);
+  	// var input=$(this);
+    // var re = /(?!^\d+$)^.+$/;
+    // if (input.val()<1 || input.val()>1000 || input.val().match(re) ){
+    //   $("#submitButton").prop('disabled', true);
+    //   input.popover('show');}
+    // else {
+    //   $("#submitButton").prop('disabled', false);
+    //   input.popover('hide');
+    // }
+  }); // end of input event listener
+  $("#form-Tix").submit(function(event) {
+    event.preventDefault();
+    console.log('purchase called');
 
+  });
 });
